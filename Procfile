146bfc55d5b1969cdc709b0cbb5b981833bee795
WendyAchieng01 <wendyachieng98@gmail.com>
2025-01-16 13:13:38 +0300

    Replace Daphne with Gunicorn for WebSocket support again2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne freelance.asgi:application
+web: daphne freelance.asgi:application -b 0.0.0.0 -p $PORT
--- freelance/asgi.py
+++ freelance/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance.settings')
+django.setup()  # Add this line before importing anything that uses Django models
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from core.routing import websocket_urlpatterns  
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance.settings')
+from core.routing import websocket_urlpatterns
 
 http_application = get_asgi_application()
 
