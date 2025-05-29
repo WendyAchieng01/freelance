a8962e685d6d2429bf496080533707f3f022ce0e
g3 <amokenathaniel@gmail.com>
2025-05-29 10:08:08 +0300

    updated Login and Logout apiviews

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken,TokenError
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import JSONParser
 from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
 
 from django.core.mail import EmailMultiAlternatives
@@ -20,7 +21,7 @@ from core.models import Job, Response as CoreResponse, Chat, Message, MessageAtt
 from ..permissions import IsClient, IsFreelancer, IsJobOwner, IsChatParticipant, CanReview
 
 from .serializers import (
-    UserSerializer, RegisterSerializer, LoginSerializer,
+    UserSerializer, RegisterSerializer, LoginSerializer,LogoutSerializer,
     PasswordChangeSerializer, PasswordResetRequestSerializer,
     PasswordResetConfirmSerializer, ProfileSerializer, SkillSerializer,
     LanguageSerializer, FreelancerProfileSerializer, ClientProfileSerializer,
@@ -196,34 +197,38 @@ class LoginView(APIView):
         if serializer.is_valid():
             user = serializer.validated_data['user']
             refresh = RefreshToken.for_user(user)
+
             return Response({
                 "access": str(refresh.access_token),
                 "refresh": str(refresh),
-                "user_type": user.profile.user_type
+                "user_type": getattr(user.profile, 'user_type', 'N/A')
             }, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class LogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     @extend_schema(
-        request={'refresh': 'string'},
+        request=LogoutSerializer,
         responses={
             205: OpenApiResponse(description="Logged out successfully."),
-            400: OpenApiResponse(description="Invalid refresh token.")
+            400: OpenApiResponse(description="Invalid or expired refresh token.")
         },
         description="Invalidate JWT refresh token."
     )
     def post(self, request):
-        try:
-            refresh_token = request.data.get("refresh")
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response({"message": "Logged out successfully."}, status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer = LogoutSerializer(data=request.data)
+        if serializer.is_valid():
+            refresh_token = serializer.validated_data['refresh']
+            try:
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+                return Response({"message": "Logged out successfully."}, status=status.HTTP_205_RESET_CONTENT)
+            except TokenError:
+                return Response({"error": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PasswordChangeView(APIView):
     permission_classes = [permissions.IsAuthenticated]
