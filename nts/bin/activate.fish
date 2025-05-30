3226c06325b0e0c9ffbf1c195d364f903cf4c5da
WendyAchieng01 <wendyachieng98@gmail.com>
2025-05-30 21:04:03 +0300

    adding migrations

--- nts/bin/activate.fish
+++ nts/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/wendy/Downloads/freelance/nts
+set -gx VIRTUAL_ENV /home/wendy/Videos/freelance/nts
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
