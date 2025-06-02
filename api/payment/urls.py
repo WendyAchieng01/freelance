b19f3f8b662ea563272eaf29eb830080a4801550
g3 <amokenathaniel@gmail.com>
2025-06-02 10:33:38 +0300

    Updates payment api to automatically redirect upon successful initialization on paystack and manuall fallback

--- api/payment/urls.py
+++ api/payment/urls.py
@@ -4,6 +4,6 @@ from .views import PaymentInitiateView, PaymentCallbackView
 
 urlpatterns = [
     path('payment/initiate/<slug:slug>/',PaymentInitiateView.as_view(), name='payment-initiate-slug'),
-    path('payment/initiate/id/<int:id>/', PaymentInitiateView.as_view(), name='payment-initiate-id'),
+    path('payment/initiate/<int:id>/', PaymentInitiateView.as_view(), name='payment-initiate-id'),
     path('payment/callback/', PaymentCallbackView.as_view(), name='payment-callback'),
 ]
