a376384711a0625845947f92fc614530b884e976
g3 <amokenathaniel@gmail.com>
2025-06-04 15:53:55 +0300

    Removed duplicate wallet urls

--- api/wallet/urls.py
+++ api/wallet/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import WalletTransactionListView, WalletSummaryView
 
 urlpatterns = [
-    path('transactions/list/', WalletTransactionListView.as_view(), name='wallet-list-transactions'),
     path('transactions/', WalletTransactionListView.as_view(),name='wallet-transactions'),
     path('summary/', WalletSummaryView.as_view(), name='wallet-summary'),
 ]
