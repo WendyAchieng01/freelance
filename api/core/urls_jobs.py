e5a65f5cbb9ccfc973d4b7386e2aa80cf1c05611
g3 <amokenathaniel@gmail.com>
2025-06-04 12:50:36 +0300

    UPdated response to apply or unapply

--- api/core/urls_jobs.py
+++ api/core/urls_jobs.py
@@ -1,26 +1,21 @@
 from django.urls import path
-from .views import JobViewSet, ResponseViewSet
+from .views import JobViewSet, ApplyToJobView,UnapplyFromJobView
 
 job_list = JobViewSet.as_view({'get': 'list', 'post': 'create'})
 job_detail = JobViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
 job_matches = JobViewSet.as_view({'get': 'matches'})
 job_complete = JobViewSet.as_view({'post': 'mark_completed'})
 
-response_list = ResponseViewSet.as_view({'get': 'list', 'post': 'create'})
-response_detail = ResponseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
-response_accept = ResponseViewSet.as_view({'patch': 'accept'})
-response_reject = ResponseViewSet.as_view({'post': 'reject'})
 
 
 urlpatterns = [
     path('list/', job_list, name='job-list'),
     path('<slug:slug>/', job_detail, name='job-detail-slug'),
     path('<int:id>/', job_detail, name='job-detail-id'),
-    path('<slug:slug>/matches/', job_matches, name='job-matches'),
-    path('<slug:slug>/complete/', job_complete, name='job-complete'),
+    path('<slug:slug>/', job_matches, name='job-matches'),
+    path('<slug:slug>/', job_complete, name='job-complete'),
+    
+    path('<slug:slug>/apply/', ApplyToJobView.as_view(), name='job-apply'),
+    path('<slug:slug>/unapply/', UnapplyFromJobView.as_view(), name='job-unapply'),
 
-    path('response/', response_list, name='response-list'),
-    path('response/<slug:slug>/', response_detail, name='response-detail'),
-    path('response/<slug:slug>/accept/', response_accept, name='response-accept'),
-    path('response/<slug:slug>/reject/', response_reject, name='response-reject'),
 ]
