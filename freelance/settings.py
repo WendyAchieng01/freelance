3a41e52013480949d935b180f78a1808c0723a4f
wessnathan <amokenathaniel@gmail.com>
2025-08-27 18:11:05 +0300

    Updated email

--- freelance/settings.py
+++ freelance/settings.py
@@ -233,13 +233,13 @@ PAYPAL_URL = (
 
 
 # Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = "smartmindtechpro@gmail.com"
-EMAIL_HOST_PASSWORD = "lhoxkhakifcxho"
+EMAIL_HOST_PASSWORD = "lhoxkhakifcxhoqz"
 DEFAULT_FROM_EMAIL = "smartmindtechpro@gmail.com"
 
 
