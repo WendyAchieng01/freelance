50de6df5daa93655ea28b2e48549bd06580c8e17
wessnathan <amokenathaniel@gmail.com>
2025-08-28 16:33:43 +0300

    Updated Settings

--- freelance/settings.py
+++ freelance/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import ssl
@@ -21,7 +21,7 @@ import dj_database_url
 #from decouple import config
 #from api.spectacular_settings import ENUM_NAME_OVERRIDES
 
-#load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-g)!b+mp+adw_fc1r-$fq2gd1os(6-!6e=fbpsd6!j0)7b-kek0'
 
-DEBUG = True #os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     '2782b2720510.ngrok-free.app',
