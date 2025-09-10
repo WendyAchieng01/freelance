3c958d3df61784c6f75362977a68a7e978833a22
wessnathan <amokenathaniel@gmail.com>
2025-09-11 01:07:49 +0300

    Redis Connect

--- freelance/celery.py
+++ freelance/celery.py
@@ -1,10 +1,9 @@
-# nilltech/celery.py
 import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance.settings')
 
-app = Celery('freelance')
+app = Celery('freelance', broker=os.environ.get('REDIS_URL'))
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
